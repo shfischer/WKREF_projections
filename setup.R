c25e8529360389aab7bf4088576c522c01aaa67e
shfischer <32540427+shfischer@users.noreply.github.com>
2022-01-04 18:08:58 +0000

    script for setting up projections

---
+++ setup.R
@@ -0,0 +1,30 @@
+### ------------------------------------------------------------------------ ###
+### use renv  ####
+### ------------------------------------------------------------------------ ###
+### replicate exact R package environment
+
+### prepare renv package
+install.packages("renv")
+renv::activate()
+
+### install required packages from lock file
+renv::restore()
+
+### ------------------------------------------------------------------------ ###
+### manual installation of R packages ####
+### ------------------------------------------------------------------------ ###
+### use only if renv does not work
+
+
+install.packages("remotes")
+install.packages("ggplot2")
+install.packages("doParallel")
+
+remotes::install_github("fishfollower/SAM/stockassessment", 
+                        INSTALL_opts = "--no-multiarch")
+
+remotes::install_github("flr/FLCore", INSTALL_opts = "--no-multiarch")
+remotes::install_github("flr/FLash", INSTALL_opts = "--no-multiarch")
+remotes::install_github("flr/ggplotFL", INSTALL_opts = "--no-multiarch")
+remotes::install_github("shfischer/FLfse/FLfse", INSTALL_opts = "--no-multiarch")
+
